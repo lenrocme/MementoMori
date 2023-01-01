bc427ad0733b112c526d9522f8357957fcc9ee24
lernocme <mcornelius@mail.ru>
2023-01-01 21:04:12 +0100

    view model for the chart

---
+++ app/src/main/java/com/example/mementomori/screen/MainChartViewModel.kt
@@ -0,0 +1,11 @@
+package com.example.mementomori.screen
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+
+class MainChartViewModel: ViewModel() {
+
+
+}
