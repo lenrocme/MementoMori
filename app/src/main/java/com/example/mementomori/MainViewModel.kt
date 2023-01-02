a2c7ff0f874c64ba17b801c50bd104ceb9082dff
lernocme <mcornelius@mail.ru>
2023-01-02 19:22:53 +0100

    add view models to main view model

--- app/src/main/java/com/example/mementomori/MainViewModel.kt
+++ app/src/main/java/com/example/mementomori/MainViewModel.kt
@@ -2,11 +2,14 @@ package com.example.mementomori
 
 import androidx.lifecycle.ViewModel
 import com.example.mementomori.data.const.AgeGroups
+import com.example.mementomori.data.const.DataPickDropDown
 import com.example.mementomori.screen.mainChart.UserDataViewModel
+import com.example.mementomori.screen.modal.userDataInput.UserInputViewModel
 
 class MainViewModel(
     val userDataVM: UserDataViewModel = UserDataViewModel(),
-    val ageGroup: AgeGroups = AgeGroups(),
+    val userInputVm: UserInputViewModel = UserInputViewModel(),
 ): ViewModel() {
-
+    val ageGroup: AgeGroups = AgeGroups()
+    val dataPick: DataPickDropDown = DataPickDropDown()
 }
