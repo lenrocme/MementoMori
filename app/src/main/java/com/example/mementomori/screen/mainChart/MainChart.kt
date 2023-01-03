0fdadcf818346fb1df9a90aea28c2f7fba7b2cb1
lernocme <mcornelius@mail.ru>
2023-01-03 15:59:43 +0100

    delete toast

--- app/src/main/java/com/example/mementomori/screen/mainChart/MainChart.kt
+++ app/src/main/java/com/example/mementomori/screen/mainChart/MainChart.kt
@@ -1,6 +1,9 @@
 package com.example.mementomori.screen.mainChart
 
+import android.widget.Toast
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CornerSize
@@ -12,9 +15,11 @@ import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.currentCompositionLocalContext
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.example.mementomori.MainViewModel
@@ -36,6 +41,7 @@ private const val itemPerRow = 15
 
 @Composable
 fun MainChart(mainVm: MainViewModel) {
+    mainVm.userDataVM.setData(mainVm.userInputVm)
     Box(modifier = Modifier
         .fillMaxSize(),
         content = {
@@ -61,7 +67,7 @@ fun MainChart(mainVm: MainViewModel) {
 }
 
 @Composable
-fun Chart(mainVm: MainViewModel, countMonths: Int = 12 * 80) {
+fun Chart(mainVm: MainViewModel) {
     var countMonth = 0
     val state = rememberScrollState()
 
@@ -70,7 +76,7 @@ fun Chart(mainVm: MainViewModel, countMonths: Int = 12 * 80) {
             .fillMaxSize()
             .verticalScroll(state),
         content = {
-            for (row in 0 until mainVm.userDataVM.userLifeExpectation / itemPerRow){
+            for (row in 0 .. mainVm.userDataVM.userLifeExpectation / itemPerRow){
                 Row(
                     modifier = Modifier
                         .fillMaxWidth(),
@@ -78,9 +84,8 @@ fun Chart(mainVm: MainViewModel, countMonths: Int = 12 * 80) {
                 ){
                     for (month in 1..itemPerRow){
                         countMonth++
-                        if(mainVm.userDataVM.userLifeExpectation < countMonth)
-                            break
-                        ChartItem(mainVm, countMonth)
+                        if(mainVm.userDataVM.userLifeExpectation >= countMonth)
+                            ChartItem(mainVm, countMonth)
                     }
                 }
                 Spacer(Modifier.height(3.dp))
@@ -91,14 +96,14 @@ fun Chart(mainVm: MainViewModel, countMonths: Int = 12 * 80) {
 @Composable
 fun ChartItem(mainVm: MainViewModel, itemMonth: Int) {
     val sizeItem = 100 / itemPerRow / 100
+    val context = LocalContext.current
     Box(
         modifier = Modifier
             .background(color = getColorBgForItemByAgeGroup(mainVm.ageGroup, itemMonth)),
         content = {
             Icon(
                 modifier = Modifier
-                    .size(percentWidth(1f) / itemPerRow
-                    ),
+                    .size(percentWidth(1f) / itemPerRow),
                 contentDescription = "Clear",
                 painter = painterResource(id = R.drawable.leaf_svgrepo_com),
                 tint = if (itemMonth % 12 == 0)
